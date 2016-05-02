141340740cecbff7b7e29595ecbece9ad24cddfa
Aaqib Parvez <aaqib29292@gmail.com>
2016-05-02 10:43:01 +0530

    rectified Houses and Gender select dropdown option while adding and editing student

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -11,10 +11,13 @@ angular.module('school.controllers', [])
 .controller('StudentDetailsController', StudentDetailsController)
 
 
-function ClassesController($scope, $resource, $ionicModal, $ionicListDelegate, $window) {
+function ClassesController($scope, $resource, $ionicModal, $ionicPopup, $ionicListDelegate, $window) {
   var vm = this;
 
   var classResource = $resource('http://localhost:3000/api/v1/klasses/:classId?access_token=TLVMLZCHEBSBAVTQJDV5LVTB7E8S74Q4');
+
+  // var classResource = $resource('https://school-db-rails.herokuapp.com/api/v1/klasses/:classId');
+
 
   console.log("ClassesController");
 
@@ -80,18 +83,34 @@ function ClassesController($scope, $resource, $ionicModal, $ionicListDelegate, $
   vm.deleteClass = function(classes) {
     console.log("delete");
     console.log(classes);
-    classResource.delete({classId:classes.id,name:classes.name});
-    vm.classResponse.klasses.splice(vm.classResponse.klasses.indexOf(classes), 1);
+
+
+    var confirmPopup = $ionicPopup.confirm({
+     title: 'Delete Class?',
+     template: 'Are you sure you want to delete this class?'
+     });
+
+    confirmPopup.then(function(res) {
+     if(res) {
+      console.log("delete done");
+      classResource.delete({classId:classes.id,name:classes.name});
+      vm.classResponse.klasses.splice(vm.classResponse.klasses.indexOf(classes), 1);
+      }
+    });
+
   }
 };
 
 
-function SectionsController($scope, $stateParams, $resource, $ionicModal, $ionicListDelegate, $window) {
+function SectionsController($scope, $stateParams, $resource, $ionicModal, $ionicPopup, $ionicListDelegate, $window) {
     var vm = this;
 
     var classId = $stateParams.id;
 
     var sectionResource = $resource('http://localhost:3000/api/v1/klasses/:classId/sections/:sectionId?access_token=TLVMLZCHEBSBAVTQJDV5LVTB7E8S74Q4', {classId: classId}, { 'update': {method: "PUT"}});
+
+    // var sectionResource = $resource('https://school-db-rails.herokuapp.com/api/v1/klasses/:classId/sections/:sectionId', {classId: classId}, { 'update': {method: "PUT"}});
+
 
     vm.getSections = function() {
       vm.sectionResponse = sectionResource.get();
@@ -159,8 +178,18 @@ function SectionsController($scope, $stateParams, $resource, $ionicModal, $ionic
 
     vm.deleteSection = function(sections) {
       console.log("delete");
-      sectionResource.delete({classId: sections.klass_id, sectionId:sections.id, name:sections.name});
-      vm.sectionResponse.sections.splice(vm.sectionResponse.sections.indexOf(sections), 1);
+      var confirmPopup = $ionicPopup.confirm({
+       title: 'Delete Class?',
+       template: 'Are you sure you want to delete this class?'
+       });
+
+      confirmPopup.then(function(res) {
+       if(res) {
+        console.log("delete done");
+        sectionResource.delete({classId: sections.klass_id, sectionId:sections.id, name:sections.name});
+        vm.sectionResponse.sections.splice(vm.sectionResponse.sections.indexOf(sections), 1);
+        }
+      });
     }
 
   }
@@ -172,6 +201,9 @@ function SectionsController($scope, $stateParams, $resource, $ionicModal, $ionic
     var sectionId = $stateParams.id;
 
     var studentResource = $resource('http://localhost:3000/api/v1/klasses/:classId/sections/:sectionId/students/:studentId?access_token=TLVMLZCHEBSBAVTQJDV5LVTB7E8S74Q4',{classId:  classId, sectionId: sectionId}, { 'update': {method: "PUT"}});
+
+    // var studentResource = $resource('https://school-db-rails.herokuapp.com/api/v1/klasses/:classId/sections/:sectionId/students/:studentId',{classId:  classId, sectionId: sectionId}, { 'update': {method: "PUT"}});
+
 
     console.log("StudentsController");
 
@@ -193,11 +225,11 @@ function SectionsController($scope, $stateParams, $resource, $ionicModal, $ionic
 
     vm.addStudent = function(name, rollnumber, fathername, gender, email, phone, dob, address, house) {
       console.log("Add");
-      console.log(rollnumber);
       studentResource.save({name: name, roll_number: rollnumber, fathers_name: fathername, gender: gender, email: email, phone: phone, dob: dob, address: address, house_id: house}).$promise.then(function(result) {
           console.log("Success");
           vm.getStudents();
       });
+
       // vm.studentResponse.students.push({name: name, roll_number: rollnumber, fathers_name: fathername, gender: gender, email: email, phone: phone, dob: dob, address: address, house_id: house})
       // work on resetting the modal
       $scope.modal.hide();
@@ -222,6 +254,9 @@ function SectionsController($scope, $stateParams, $resource, $ionicModal, $ionic
 
     var studentDetailsResource = $resource('http://localhost:3000/api/v1/klasses/:classId/sections/:sectionId/students/:studentId?access_token=TLVMLZCHEBSBAVTQJDV5LVTB7E8S74Q4',{classId:  classId, sectionId: sectionId, studentId: studentId}, { 'update': {method: "PUT"}});
 
+    // var studentDetailsResource = $resource('https://school-db-rails.herokuapp.com/api/v1/klasses/:classId/sections/:sectionId/students/:studentId', {classId:  classId, sectionId: sectionId, studentId: studentId}, { 'update': {method: "PUT"}});
+
+
     vm.getStudents = function() {
       vm.studentResponse = studentDetailsResource.get();
       console.log(vm.studentResponse);
@@ -242,12 +277,39 @@ function SectionsController($scope, $stateParams, $resource, $ionicModal, $ionic
       console.log("Edit");
       $scope.modal2.show();
       vm.student = student;
+      vm.student.dob = new Date(student.dob);
+      console.log("student id: " + student.house_id);
+      console.log("student gender_id: " + student.gender_id);
+      vm.houses = [
+        {id:'1', name:'Yellow' },
+        {id:'2', name:'Red' },
+        {id:'3', name:'Blue' },
+        {id:'4', name:'Green' },
+      ];
+      vm.genders = [
+        {id:'1', code:'Male' },
+        {id:'2', code:'Female' }
+      ];
+
+      vm.student.gender_id = vm.genders[student.gender_id - 1];
+
+      vm.student.house_id = vm.houses[student.house_id - 1];
     }
 
-    vm.updateStudent = function(student, name, rollnumber, fathername, gender, email, phone, dob, address, house) {
+    vm.cancelModal = function(){
+      vm.getStudents();
       $scope.modal2.hide();
-      studentDetailsResource.update({name: name, roll_number: rollnumber, fathers_name: fathername, gender: gender, email: email, phone: phone, dob: dob, address: address, house_id: house}, student);
+    };
+
+    vm.updateStudent = function(student) {
+      $scope.modal2.hide();
       console.log("Update");
+      console.log(student.house_id.id);
+      console.log(student.gender_id.id);
+      studentDetailsResource.update({studentId: student.id, name: student.name, roll_number: student.roll_number, fathers_name: student.fathers_name, house_id: student.house_id.id, gender: student.gender_id.id, email: student.email, phone: student.phone, dob: student.dob, address: student.address}, student).$promise.then(function(result) {
+          console.log("Success");
+          vm.getStudents();
+      });
     }
 
     vm.deleteStudent = function(student) {
